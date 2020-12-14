75da3d045136ceaffa3de7fe49d861c061f015b1
Alexandra Sandu <sandu.alexandra99@yahoo.com>
2020-12-14 18:29:00 +0200

    Lab11

---
+++ Sandu_Ana_alexandra_Lab10/Models/ListProduct.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+namespace Sandu_Ana_alexandra_Lab10.Models
+{
+    public class ListProduct
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        [ForeignKey(typeof(ShopList))]
+        public int ShopListID { get; set; }
+        public int ProductID { get; set; }
+    }
+}
---
+++ Sandu_Ana_alexandra_Lab10/ValidationBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sandu_Ana_alexandra_Lab10.Models;
+using System.IO;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+namespace Sandu_Ana_alexandra_Lab10
+{
+    class ValidationBehaviour: Behavior<Editor>
+    {
+        protected override void OnAttachedTo(Editor entry)
+        {
+            entry.TextChanged += OnEntryTextChanged;
+            base.OnAttachedTo(entry);
+        }
+        protected override void OnDetachingFrom(Editor entry)
+        {
+            entry.TextChanged -= OnEntryTextChanged;
+            base.OnDetachingFrom(entry);
+        }
+        void OnEntryTextChanged(object sender, TextChangedEventArgs args)
+        {
+            ((Editor)sender).BackgroundColor =
+            string.IsNullOrEmpty(args.NewTextValue)?Color.Red:Color.Default;
+        }
+    }
+}
